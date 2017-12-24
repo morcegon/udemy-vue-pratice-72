0513f3b40f19aec25c52c944ee2e21431e2d54a0
Renan Andrade <eu@renanandrade.com.br>
2017-12-23 23:47:52 -0200

    add server item component

---
+++ src/components/Server/ServerItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <li
+      class="list-group-item">
+      Server #{{ serverInfo.id }}
+    </li>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      serverInfo: Object
+    }
+  }
+</script>
---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <server-item
+        v-for="server in servers"
+        :serverInfo='server'
+        :key="server.id"></server-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import ServerItem from './ServerItem.vue'
+  export default {
+    data: () => {
+      return {
+        servers: [
+          {id: 1, status: 'Critical'},
+          {id: 2, status: 'Offline'},
+          {id: 3, status: 'Loaded'},
+          {id: 4, status: 'Unknow'},
+          {id: 5, status: 'Critical'}
+        ]
+      }
+    },
+    components: {
+      serverItem: ServerItem
+    }
+  }
+</script>
